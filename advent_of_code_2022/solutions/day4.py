e021721b41d22b20a6d728bd0cf9e02bbabda940
Christopher Chen <christopher.chen1995@gmail.com>
2022-12-03 21:57:47 -0800

    Whiff

--- advent_of_code_2022/solutions/day4.py
+++ advent_of_code_2022/solutions/day4.py
@@ -1,8 +1,33 @@
 from advent_of_code_2022.problem import ProblemBase
+from itertools import groupby
 
 
 def contains_fully(a_start, a_stop, b_start, b_stop):
     return a_start <= b_start and a_stop >= b_stop
+
+
+def overlaps(a_start, a_stop, b_start, b_stop):
+    """
+    a      a
+      b  b
+    a  a
+     b   b
+     a a
+    b b
+    """
+    ret = 2 < len(
+        list(
+            groupby(
+                sorted(
+                    [(0, a_start), (0, a_stop), (1, b_start), (1, b_stop)],
+                    key=lambda t: t[1],
+                ),
+                key=lambda t: t[0],
+            )
+        )
+    )
+    print(ret, a_start, a_stop, b_start, b_stop)
+    return ret
 
 
 def one_contains_another(a_start, a_stop, b_start, b_stop):
@@ -23,4 +48,8 @@ class Day4_1(ProblemBase):
 
 
 class Day4_2(Day4_1):
-    pass
+    def solve(self, data: str):
+        return sum(
+            overlaps(*[int(i) for x in line.split(",") for i in x.split("-")])
+            for line in data.split("\n")
+        )
