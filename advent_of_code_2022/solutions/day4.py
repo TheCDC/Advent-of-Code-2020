d22d084af8d17d5fe90578a65387f4c32631d206
Christopher Chen <christopher.chen1995@gmail.com>
2022-12-03 22:12:12 -0800

    Solve Day4-2

--- advent_of_code_2022/solutions/day4.py
+++ advent_of_code_2022/solutions/day4.py
@@ -1,5 +1,5 @@
 from advent_of_code_2022.problem import ProblemBase
-from itertools import groupby
+from itertools import permutations
 
 
 def contains_fully(a_start, a_stop, b_start, b_stop):
@@ -15,19 +15,13 @@ def overlaps(a_start, a_stop, b_start, b_stop):
      a a
     b b
     """
-    ret = 2 < len(
-        list(
-            groupby(
-                sorted(
-                    [(0, a_start), (0, a_stop), (1, b_start), (1, b_stop)],
-                    key=lambda t: t[1],
-                ),
-                key=lambda t: t[0],
-            )
-        )
-    )
-    print(ret, a_start, a_stop, b_start, b_stop)
-    return ret
+    pairs = [(a_start, a_stop), (b_start, b_stop)]
+
+    s = [
+        sorted(pairs, key=lambda t: t[0])[0],
+        sorted(pairs, key=lambda t: t[1], reverse=True)[0],
+    ]
+    return s[0][1] >= s[1][0]
 
 
 def one_contains_another(a_start, a_stop, b_start, b_stop):
@@ -48,6 +42,9 @@ class Day4_1(ProblemBase):
 
 
 class Day4_2(Day4_1):
+    # 723 too low
+    # try 837
+    # correct 837
     def solve(self, data: str):
         return sum(
             overlaps(*[int(i) for x in line.split(",") for i in x.split("-")])
