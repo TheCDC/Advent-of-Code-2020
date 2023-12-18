ad1ca4150d3333284c31fb973461dcbc94046726
Christopher Chen <christopher.chen1995@gmail.com>
2023-12-17 22:08:28 -0800

    Scaffold day4 and add problem input

---
+++ advent_of_code_2023/solutions/day4.py
@@ -0,0 +1,10 @@
+from advent_of_code_2023.problem import ProblemBase
+
+class Day4_1(ProblemBase):
+    def solve(self,input_str:str):
+        pass
+
+
+class Day4_2(ProblemBase):
+    def solve(self,input_str:str):
+        pass
