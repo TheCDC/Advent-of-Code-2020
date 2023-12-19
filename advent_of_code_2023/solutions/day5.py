3415fb2b3cd2487c522a742aafe8702924c41225
Christopher Chen <christopher.chen1995@gmail.com>
2023-12-19 12:01:27 -0800

    Scaffold Day 5

---
+++ advent_of_code_2023/solutions/day5.py
@@ -0,0 +1,11 @@
+from advent_of_code_2023.problem import ProblemBase
+
+
+class Day5_1(ProblemBase):
+    def solve(self, input_str: str):
+        pass
+
+
+class Day5_2(ProblemBase):
+    def solve(self, input_str: str):
+        pass
