2c36081d3e745bd81815e985e8076ae6bec8c30f
Christopher Chen <christopher.chen1995@gmail.com>
2024-12-25 17:50:05 -0800

    Solve Day2-2

--- advent_of_code_2024/solutions/day2.py
+++ advent_of_code_2024/solutions/day2.py
@@ -4,12 +4,17 @@ from advent_of_code_2024.problem import ProblemBase
 def check(report: list[int]):
     # True=Safe
     deltas = [b - a for a, b in zip(report, report[1:])]
-    print(deltas)
     return all(abs(d) >= 1 and abs(d) <= 3 for d in deltas) and (
         all(d > 0 for d in deltas) or all(d < 0 for d in deltas)
     )
-
-
+def check_count(report: list[int]):
+    # True=Safe
+    if check(report):
+        return True
+    for index in range(len(report)):
+        if check(report[:index]+report[index+1:]):
+            return True
+    return False
 class Day2_1(ProblemBase):
     def solve(self, input_str: str):
         reports = [[int(i) for i in line.split()] for line in input_str.split("\n")]
@@ -18,4 +23,7 @@ class Day2_1(ProblemBase):
 
 class Day2_2(ProblemBase):
     def solve(self, input_str: str):
-        pass
+        reports = [[int(i) for i in line.split()] for line in input_str.split("\n")]
+        return sum(check_count(r) for r in reports)
+
+
